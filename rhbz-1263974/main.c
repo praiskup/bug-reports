0cfa8a8e8ebf1d73f8a7ea7faf9421771a0981dd
Pavel Raiskup <praiskup@redhat.com>
2016-09-26 12:15:05 +0200

    rhbz 1263974 - verbose output
    $ make && MALLOC_MMAP_THRESHOLD_=524288 ./program 15 5000
    make: Nothing to be done for 'all'.
     * first malloc starts at: 0xf74e6008
    using 15 maps and 5000 mallocs
     * started mmaping from 0xeace6000
     * after-mmap malloc() starts from 0x0926e410
     * after-mmap malloc() ends on 0xf9eb8008
    My PID is 8928
    00048000-08048000 rw-p 00000000 00:00 0
    // I'm not sure what this is for ^^
    08048000-08049000 r-xp 00000000 00:25 3325390  /home/praiskup/rh/projects/bug-reports/rhbz-1263974/program
    08049000-0804a000 r--p 00000000 00:25 3325390  /home/praiskup/rh/projects/bug-reports/rhbz-1263974/program
    0804a000-0804b000 rw-p 00001000 00:25 3325390  /home/praiskup/rh/projects/bug-reports/rhbz-1263974/program
    0806e000-99893000 rw-p 00000000 00:00 0        [heap]
    // some mallocs reside here ^^^
    998e6000-9fce6000 rw-s 00000000 00:13 425911   /dev/shm/test-shm-mapping-14
    9fce6000-a60e6000 rw-s 00000000 00:13 425910   /dev/shm/test-shm-mapping-13
    a60e6000-ac4e6000 rw-s 00000000 00:13 425909   /dev/shm/test-shm-mapping-12
    ac4e6000-b28e6000 rw-s 00000000 00:13 425908   /dev/shm/test-shm-mapping-11
    b28e6000-b8ce6000 rw-s 00000000 00:13 425907   /dev/shm/test-shm-mapping-10
    b8ce6000-bf0e6000 rw-s 00000000 00:13 425906   /dev/shm/test-shm-mapping-9
    bf0e6000-c54e6000 rw-s 00000000 00:13 425905   /dev/shm/test-shm-mapping-8
    c54e6000-cb8e6000 rw-s 00000000 00:13 425904   /dev/shm/test-shm-mapping-7
    cb8e6000-d1ce6000 rw-s 00000000 00:13 425903   /dev/shm/test-shm-mapping-6
    d1ce6000-d80e6000 rw-s 00000000 00:13 425902   /dev/shm/test-shm-mapping-5
    d80e6000-de4e6000 rw-s 00000000 00:13 425901   /dev/shm/test-shm-mapping-4
    de4e6000-e48e6000 rw-s 00000000 00:13 425900   /dev/shm/test-shm-mapping-3
    e48e6000-eace6000 rw-s 00000000 00:13 425899   /dev/shm/test-shm-mapping-2
    eace6000-f10e6000 rw-s 00000000 00:13 425898   /dev/shm/test-shm-mapping-1
    f10e6000-f74e6000 rw-s 00000000 00:13 425897   /dev/shm/test-shm-mapping-0
    f74e6000-f7569000 rw-p 00000000 00:00 0
    f7569000-f7581000 r-xp 00000000 00:25 2117049  /usr/lib/libpthread-2.23.so
    f7581000-f7582000 r--p 00017000 00:25 2117049  /usr/lib/libpthread-2.23.so
    f7582000-f7583000 rw-p 00018000 00:25 2117049  /usr/lib/libpthread-2.23.so
    f7583000-f7585000 rw-p 00000000 00:00 0
    f7585000-f774d000 r-xp 00000000 00:25 2117023  /usr/lib/libc-2.23.so
    f774d000-f774e000 ---p 001c8000 00:25 2117023  /usr/lib/libc-2.23.so
    f774e000-f7750000 r--p 001c8000 00:25 2117023  /usr/lib/libc-2.23.so
    f7750000-f7751000 rw-p 001ca000 00:25 2117023  /usr/lib/libc-2.23.so
    f7751000-f7754000 rw-p 00000000 00:00 0
    f7754000-f775b000 r-xp 00000000 00:25 2117053  /usr/lib/librt-2.23.so
    f775b000-f775c000 r--p 00006000 00:25 2117053  /usr/lib/librt-2.23.so
    f775c000-f775d000 rw-p 00007000 00:25 2117053  /usr/lib/librt-2.23.so
    f778f000-f7791000 r--p 00000000 00:00 0        [vvar]
    f7791000-f7793000 r-xp 00000000 00:00 0        [vdso]
    f7793000-f77b5000 r-xp 00000000 00:25 2117016  /usr/lib/ld-2.23.so
    f77b5000-f77b6000 rw-p 00000000 00:00 0
    f77b6000-f77b7000 r--p 00022000 00:25 2117016  /usr/lib/ld-2.23.so
    f77b7000-f77b8000 rw-p 00023000 00:25 2117016  /usr/lib/ld-2.23.so
    f77b8000-f9fb8000 rw-p 00000000 00:00 0
    // first malloc comes here ^^^, same the last one, should be
    // marked [heap] too.
    ffc29000-ffc4b000 rw-p 00000000 00:00 0        [stack]

--- rhbz-1263974/main.c
+++ rhbz-1263974/main.c
@@ -23,6 +23,7 @@ void my_mmap(int id)
 {
     char filename[100];
     sprintf(filename, "/test-shm-mapping-%d", id);
+    void *ptr;
 
     shm_unlink(filename);
     int fd = shm_open(filename, O_RDWR | O_CREAT | O_EXCL, 0600);
@@ -32,7 +33,10 @@ void my_mmap(int id)
     if (ftruncate(fd, SHM_BLOCK_SIZE) == -1)
         die("ftruncate fail");
 
-    mmap(NULL, SHM_BLOCK_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+    ptr = mmap(NULL, SHM_BLOCK_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (id == 1)
+        printf (" * started mmaping from %.8p\n", ptr);
 }
 
 int main(int argc, char **argv)
@@ -40,6 +44,7 @@ int main(int argc, char **argv)
     char errmsg[70];
     char cmd[100];
     char *endptr = NULL;
+    void * ptr;
 
     if (argc < 3)
         die("COUNT_MAPS(100M) COUNT_MALLOCS(511k) args expected");
@@ -49,6 +54,8 @@ int main(int argc, char **argv)
         sprintf(errmsg, "'%s' is not valid number", argv[1]);
         die(errmsg);
     }
+
+    printf(" * first malloc starts at: %.8p\n", malloc(512*1024));
 
     int count_mallocs = strtol(argv[2], &endptr, 10);
     if (!endptr || *endptr) {
@@ -63,8 +70,11 @@ int main(int argc, char **argv)
 
     // this is to have [heap] mapping
     for (int i = 0; i < count_mallocs; i++) {
-        malloc(511*1024);
+        ptr = malloc(511*1024);
+        if (i == 0)
+            printf(" * after-mmap malloc() starts from %.8p\n", ptr);
     }
+    printf(" * after-mmap malloc() ends on %.8p\n", ptr);
 
     printf("My PID is %d\n", getpid());
 
